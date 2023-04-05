a8feb0c51d4f69a48dbfac24bed865817a050c54
AnasNaamneh <naamnih.anas@gmail.com>
2023-04-04 18:01:11 -0700

    Add new Functions To Library

--- String Library Project/String Library Project.cpp
+++ String Library Project/String Library Project.cpp
@@ -21,19 +21,19 @@ int main()
 
 	cout<< "Number of Words = " << clsString::CountWords("Anas Wajeh Naamneh");
 
-	vector <string> S1 = String1.SplitString();
+	vector <string> S1 = String1.Split();
 
 	for (string & S : S1) {
 		cout << "Word After Split: " << S << endl;
 	}
 
-	vector <string> S2 = String1.SplitString("anas#//#naamneh","#//#");
+	vector <string> S2 = String1.Split("anas#//#naamneh","#//#");
 
 	for (string& S : S2) {
 		cout << "Word After Split: " << S << endl;
 	}
 
-	       vector <string> S3 = clsString::SplitString("Omar$$Ameen" , "$$");
+	       vector <string> S3 = clsString::Split("Omar$$Ameen" , "$$");
 
 
 		   for (string& S : S3) {
@@ -66,14 +66,20 @@ int main()
 
 	String2.Value = "i love programming";
 
-	cout << "Upper First Character: " << String2.PrintUpperFirstLetterOfEachWord() << endl;
-	cout << "Upper First Character: " << String2.PrintUpperFirstLetterOfEachWord("welcome to my project") << endl;
-	cout << "Upper First Character: " << clsString::PrintUpperFirstLetterOfEachWord("welcome to my project") << endl;
+	cout << "Upper First Character: " << endl;
+	String2.PrintUpperFirstLetterOfEachWord();
+	cout << "Upper First Character: " << endl;
+	String2.PrintUpperFirstLetterOfEachWord("welcome to my project");
+	cout << "Upper First Character: " << endl;
+	clsString::PrintUpperFirstLetterOfEachWord("welcome to my project");
 
 
-	cout << "Lower First Character: " << String2.PrintLowerFirstLetterOfEachWord() << endl;
-	cout << "Lower First Character: " << String2.PrintLowerFirstLetterOfEachWord("WELCOME") << endl;
-	cout << "Lower First Character: " << clsString::PrintLowerFirstLetterOfEachWord("PRO") << endl;
+	cout << "Lower First Character: " << endl;
+	String2.PrintLowerFirstLetterOfEachWord();
+	cout << "Lower First Character: " << endl;
+	String2.PrintLowerFirstLetterOfEachWord("WELCOME");
+	cout << "Lower First Character: " << endl;
+	clsString::PrintLowerFirstLetterOfEachWord("PRO");
 
 
 	cout << "Print Each Word: " << endl;
@@ -109,6 +115,45 @@ int main()
 	cout << "Count Small Letters: " << String2.CountSmallLetters("HELLO") << endl;
 	cout << "Count Small Letters: " << clsString::CountSmallLetters("hello") << endl;
 
+	cout << "Count Vowel Letters: " << String2.CountVowels() << endl;
+	cout << "Count Vowel Letters: " << String2.CountVowels("HELLO") << endl;
+	cout << "Count Vowel Letters: " << clsString::CountVowels("cat") << endl;
+
+
+	cout << "String Length: " << String2.Length() << endl;
+	cout << "String Length: " << String2.Length("ahmed") << endl;
+	cout << "String Length: " << clsString::Length("programming") << endl;
+
+
+	cout << String2.InvertLetterCase('a') << endl;
+	cout << clsString::InvertLetterCase('A') << endl;
+
+	cout << "String Invert ALl: ";
+	String1.InvertAllLetterCases();
+	cout << "String Invert All: " << String2.InvertAllLetterCases("ahmed") << endl;
+	cout << "String Invert All: " << clsString::InvertAllLetterCases("programming") << endl;
+
+
+	cout << "Count Specific Letters: " << String1.CountSpecificLetters("anas", 'a', true) << endl;
+	cout << "Count Specific Letters: " << String1.CountSpecificLetters("ahmed", 'D', false) << endl;
+	cout << "Count Specific Letters: " << clsString::CountSpecificLetters("ahmed", 'D', true) << endl;
+
+	cout << "Trim Left: " << String1.TrimLeft() << endl;
+	cout << "Trim Left: " << String1.TrimLeft("Www") << endl;
+	cout << "Trim Left: " << clsString::TrimLeft("www") << endl;
+
+	cout << "Trim Right: " << String1.TrimRight() << endl;
+	cout << "Trim Right: " << String1.TrimRight("Www") << endl;
+	cout << "Trim Right: " << clsString::TrimRight("www") << endl;
+
+	String1.Value = "anas naamneh";
+
+	cout << "Trim: " << endl;
+	String1.Trim();
+	cout << "Trim: " << String1.Trim("hello") << endl;
+	cout << "Trim: " << clsString::Trim("hello") << endl;
+
+	cout << String1.ReplaceString("anas" ,"ahmad") << endl;
 
 	system("pause>0");
 	return 0;
--- String Library Project/clsString.h
+++ String Library Project/clsString.h
@@ -10,12 +10,6 @@ class clsString
 private:
 	string _Value;
 
-   static bool isVowel(char Ch1)
-    {
-        Ch1 = tolower(Ch1);
-        return (Ch1 == 'a') || (Ch1 == 'o') || (Ch1 == 'u') || (Ch1 == 'e') || (Ch1 == 'i');
-    }
-
 public:
 
 	clsString() {
@@ -36,6 +30,10 @@ public:
 	}
 
 	__declspec(property(get = GetValue , put = SetValue)) string Value;
+
+
+
+   
 
    static short CountWords(string S1)
     {
@@ -64,7 +62,7 @@ public:
         return CountWords(_Value);
     }
 
-   static vector<string> SplitString(string S1, string delim)
+   static vector<string> Split(string S1, string delim)
     {
         short pos = 0;
         string sWord = "";
@@ -86,8 +84,136 @@ public:
     }
 
 
-    vector <string> SplitString() {
-        return SplitString(_Value, " ");
+    vector <string> Split() {
+        return Split(_Value, " ");
+    }
+
+
+   static string JoinString(vector <string> vString, string delim) {
+
+        string S1 = "";
+        for (string& s : vString) {
+
+
+            S1 = S1 + s + delim;
+
+        }
+        return S1.substr(0, S1.length() - delim.length());
+    }
+
+
+    static string JoinString(string arrString[], short Length, string delim) {
+
+        string S1 = "";
+        for (short i = 0; i < Length; i++) {
+
+
+            S1 = S1 + arrString[i] + delim;
+
+        }
+        return S1.substr(0, S1.length() - delim.length());
+    }
+
+   static string ReplaceString(string S1, string StringToReplace, string ReplaceTo, bool MatchCase = true) {
+
+        vector <string> vString;
+
+
+        vString = Split(S1, " ");
+
+        for (string& s : vString) {
+
+            if (MatchCase) {
+
+                if (s == StringToReplace) {
+
+                    s = ReplaceTo;
+                }
+
+            }
+            else {
+
+                if (LowerAllString(s) == LowerAllString(StringToReplace)) {
+
+                    s = ReplaceTo;
+                }
+
+
+            }
+
+        }
+        return JoinString(vString, " ");
+    }
+
+
+    string ReplaceString(string StringToReplace, string ReplaceTo, bool MatchCase = true) {
+
+        return ReplaceString(_Value , StringToReplace, ReplaceTo, MatchCase);
+
+    }
+
+
+
+    static string TrimLeft(string S1)
+    {
+        for (int i = 0; i < S1.length(); i++)
+        {
+
+            if (S1[i] != ' ') {
+
+                return  S1.substr(i, S1.length() - i);
+            }
+
+        }
+        return "";
+
+    }
+
+     string TrimLeft()
+    {
+         return TrimLeft(_Value);
+    }
+
+     static string TrimRight(string S1)
+     {
+         for (int i = S1.length() - 1; i >= 0; i--)
+         {
+             if (S1[i] != ' ') {
+
+                 return S1.substr(0, i + 1);
+
+             }
+
+         }
+         return "";
+
+     }
+
+    string TrimRight()
+     {
+         return TrimRight(_Value);
+     }
+
+    static string Trim(string S1) {
+        return  TrimLeft(TrimRight(S1));
+
+    }
+
+     void Trim() {
+
+        TrimLeft(TrimRight(_Value));
+
+     }
+
+
+    static short Length(string S1) {
+        
+        return S1.length();
+    }
+
+
+    short Length() {
+        return _Value.length();
     }
 
 
@@ -126,7 +252,7 @@ public:
         vector <string> vString;
         string S2 = "";
 
-        vString = SplitString(S1, " ");
+        vString = Split(S1, " ");
         vector <string>::iterator iter = vString.end();
 
         while (iter != vString.begin()) {
@@ -158,8 +284,6 @@ public:
 
                  S2 += S1[i];
              }
-
-
          }
 
          return S2;
@@ -186,9 +310,9 @@ public:
          return S1;
      }
 
-     string PrintUpperFirstLetterOfEachWord()
+     void PrintUpperFirstLetterOfEachWord()
      {
-         return PrintUpperFirstLetterOfEachWord(_Value);
+         _Value = PrintUpperFirstLetterOfEachWord(_Value);
      }
 
 
@@ -207,13 +331,36 @@ public:
      }
 
 
-     string PrintLowerFirstLetterOfEachWord()
+     void PrintLowerFirstLetterOfEachWord()
      {
-         return PrintLowerFirstLetterOfEachWord(_Value);
+         _Value = PrintLowerFirstLetterOfEachWord(_Value);
+     }
+
+    static string InvertAllLetterCases(string S1)
+     {
+
+         for (short i = 0; i < S1.length(); i++)
+         {
+             S1[i] = InvertLetterCase(S1[i]);
+
+         }
+
+         return S1;
+     }
+
+    void InvertAllLetterCases() {
+
+        _Value = InvertAllLetterCases(_Value);
+    }
+
+    static char InvertLetterCase(char Ch1)
+     {
+         return isupper(Ch1) ? tolower(Ch1) : toupper(Ch1);
+
      }
 
 
-
+  
 
 
     static void PrintEachWordInString(string S1)
@@ -267,6 +414,51 @@ public:
          return PrintAllVowelsInString(_Value);
      }
 
+    static short CountSpecificLetters(string S1, char Ch1, bool MatchCase = true)
+     {
+         short Count = 0;
+         for (short i = 0; i < S1.length(); i++)
+         {
+             if (MatchCase)
+             {
+                 if (Ch1 == S1[i]) {
+                     Count++;
+                 }
+             }
+             else
+             {
+                 if (tolower(Ch1) == tolower(S1[i]))
+                 {
+                     Count++;
+                 }
+             }
+         }
+         return Count;
+     }
+
+   
+
+
+   static short CountVowels(string S1)
+     {
+         short Count = 0;
+         for (short i = 0; i < S1.length(); i++)
+         {
+             if (isVowel(S1[i]))
+             {
+                 Count++;
+             }
+         }
+         return Count;
+     }
+
+     short CountVowels()
+     {
+         return CountVowels(_Value);
+     }
+
+
+
     static short CountCapitalLetters(string S1)
      {
          short Count = 0;
@@ -302,6 +494,12 @@ public:
      short CountSmallLetters()
      {
          return CountSmallLetters(_Value);
+     }
+
+     static bool isVowel(char Ch1)
+     {
+         Ch1 = tolower(Ch1);
+         return (Ch1 == 'a') || (Ch1 == 'o') || (Ch1 == 'u') || (Ch1 == 'e') || (Ch1 == 'i');
      }
 
 };
